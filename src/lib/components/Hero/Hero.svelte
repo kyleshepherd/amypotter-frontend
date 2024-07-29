15f51427e7d2fe399802443c3a8ea8c83f98ea18
Kyle Shepherd <kyle@thisissoon.com>
2024-07-29 18:05:01 +0100

    feat(Hero): Initial component

---
+++ src/lib/components/Hero/Hero.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let introText: string;
+</script>
+
+<div
+  class="flex items-center justify-center p-3 sm:p-6 md:p-9 lg:h-dvh lg:p-12"
+>
+  <div class="py-36 text-center">
+    <h1
+      class="mb-8 font-display text-6xl sm:text-7xl md:text-9xl lg:text-[10rem]"
+    >
+      Amy Potter
+    </h1>
+    <p class="text-sm md:text-base">{introText}</p>
+  </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import Hero from "$lib/components/Hero/Hero.svelte";
+</script>
+
+<Hero introText="Some intro text lorem ipsum dolor sit amet" />

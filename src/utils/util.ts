e592311002b6ecabd5ce398f057053e7048c62b8
Kyle Shepherd <kyle@thisissoon.com>
2024-11-20 22:09:22 +0000

    fix(Util): Fix handleAnchorClick for non-current-page anchors

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,3 +1,5 @@
+import { goto } from "$app/navigation";
+
 export const lerp = (a: number, b: number, alpha: number): number => {
   return a + alpha * (b - a);
 };
@@ -5,7 +7,12 @@ export const lerp = (a: number, b: number, alpha: number): number => {
 export const handleAnchorClick = (event: any) => {
   event.preventDefault();
   const link = event.currentTarget;
-  const anchorId = new URL(link.href).hash.replace("#", "");
+  const linkUrl = new URL(link.href);
+  if (linkUrl.pathname !== window.location.pathname) {
+    goto(linkUrl);
+    return;
+  }
+  const anchorId = linkUrl.hash.replace("#", "");
   const anchor = document.getElementById(anchorId);
   if (!anchor) {
     return;

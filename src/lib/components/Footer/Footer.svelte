7d1ffb5b85533dd5424092c086fdea521dff08b3
Kyle Shepherd <kyle@thisissoon.com>
2024-11-21 17:22:10 +0000

    feat(Footer): Remove instagram

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -1,8 +1,5 @@
-<script lang="ts">
-</script>
-
 <footer
-  class="flex items-center justify-between bg-black px-3 py-3 text-xs uppercase text-white sm:px-6 md:px-9 lg:px-12 xl:px-16"
+  class="bg-black px-3 py-3 text-xs uppercase text-white sm:px-6 md:px-9 lg:px-12 xl:px-16"
 >
   <div class="text-xs">
     <p>© {new Date().getFullYear()}</p>
@@ -17,12 +14,4 @@
       </a>
     </p>
   </div>
-  <a
-    href="https://www.instagram.com/amyp0tter/"
-    target="_blank"
-    rel="noopener noreferrer"
-    class="rounded-full border border-white p-2 uppercase transition-colors hover:bg-white hover:text-black"
-  >
-    Instagram
-  </a>
 </footer>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,4 +1,6 @@
 <div class="flex h-dvh w-dvw flex-col items-center justify-center text-center">
-  <h1 class="font-display text-4xl xs:text-5xl md:text-6xl">Page Not Found</h1>
+  <h1 class="font-display text-4xl xs:text-5xl md:text-6xl">
+    Hmm, that's not right.
+  </h1>
   <a href="/" class="underline">Return to home</a>
 </div>
